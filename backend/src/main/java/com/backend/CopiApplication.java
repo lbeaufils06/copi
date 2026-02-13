52b99a1f77c1618366d1378c3a40b9f6ece8e19f
lbeaufils <lowstation@hotmail.fr>
2026-02-13 12:33:33 +0100

    add config sqlLite

--- backend/src/main/java/com/backend/CopiApplication.java
+++ backend/src/main/java/com/backend/CopiApplication.java
@@ -2,12 +2,14 @@ package com.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class BackendApplication {
+@EnableScheduling
+public class CopiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(CopiApplication.class, args);
 	}
 
 }
