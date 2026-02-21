f96113eb40059ee1ec9d486be1dba00e946150f6
lbeaufils <lowstation@hotmail.fr>
2026-02-22 00:03:13 +0100

    add recoverInterruptedExecutions

--- backend/src/main/java/com/backend/copi/repository/BackupExecutionRepository.java
+++ backend/src/main/java/com/backend/copi/repository/BackupExecutionRepository.java
@@ -31,6 +31,8 @@ public interface BackupExecutionRepository extends JpaRepository<BackupExecution
 	long countByJob_IdAndStatus(UUID jobId, ExecutionStatus status);
 	
 	List<BackupExecution> findByStatusIn(List<ExecutionStatus> statuses);
+	
+	List<BackupExecution> findByStatus(ExecutionStatus status);
 
 
 }
