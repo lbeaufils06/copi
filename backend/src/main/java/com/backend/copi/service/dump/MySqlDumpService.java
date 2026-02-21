8f0670ce254ab918ab5d9d45482157db675b8d83
lbeaufils <lowstation@hotmail.fr>
2026-02-21 22:29:00 +0100

    delete command --set-gtid-purged=OFF

--- backend/src/main/java/com/backend/copi/service/dump/MySqlDumpService.java
+++ backend/src/main/java/com/backend/copi/service/dump/MySqlDumpService.java
@@ -58,7 +58,6 @@ public class MySqlDumpService implements DatabaseDumpService {
         command.add("--events");
         command.add("--triggers");
         command.add("--no-tablespaces");
-        command.add("--set-gtid-purged=OFF");
 
         if (job.getDbName() == null || job.getDbName().trim().isEmpty()) {
             command.add("--all-databases");
