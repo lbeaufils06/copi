48ee102079eaa801e4462c64362239682f95ec66
lbeaufils <lowstation@hotmail.fr>
2026-02-24 17:55:50 +0100

    autofocus password

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -54,6 +54,7 @@ export default function LoginPage() {
             autoComplete="current-password"
             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             value={password}
+            autoFocus
             onChange={(e) => setPassword(e.target.value)}
             />
 
