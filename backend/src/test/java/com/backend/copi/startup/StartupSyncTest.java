db95eeb834c993d7b8d85172ef659821b46a48bf
lbeaufils <lowstation@hotmail.fr>
2026-02-27 19:33:37 +0100

    add new test up 5

---
+++ backend/src/test/java/com/backend/copi/startup/StartupSyncTest.java
@@ -0,0 +1,42 @@
+package com.backend.copi.startup;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+
+import com.backend.copi.service.BackupJobService;
+import com.backend.copi.service.BackupStorageService;
+
+class StartupSyncTest {
+
+    private BackupStorageService backupStorageService;
+    private BackupJobService backupJobService;
+    private StartupSync startupSync;
+
+    @BeforeEach
+    void setup() {
+        backupStorageService = mock(BackupStorageService.class);
+        backupJobService = mock(BackupJobService.class);
+
+        startupSync = new StartupSync(
+                backupStorageService,
+                backupJobService
+        );
+    }
+
+    @Test
+    void onStartup_shouldCallServicesInCorrectOrder() {
+
+        startupSync.onStartup();
+
+        InOrder inOrder = inOrder(backupStorageService, backupJobService);
+
+        inOrder.verify(backupStorageService).synchronize();
+        inOrder.verify(backupJobService).recoverInterruptedExecutions();
+        inOrder.verify(backupStorageService).deleteFailedExecutionsAndFiles();
+
+        verifyNoMoreInteractions(backupStorageService, backupJobService);
+    }
+}
